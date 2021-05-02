9bf40695b658dc12811781a8947490ecfa8c0e2d
ZhuoFan Chen <4582h@dunearn.edu.sg>
2021-05-02 12:54:29 +0000

    Sunny:Include face unlock flag and redfin fingerprint

--- aosp_mojito.mk
+++ aosp_mojito.mk
@@ -19,12 +19,21 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_mojito
+TARGET_GAPPS_ARCH := arm64
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
-
+WITH_GAPPS := true
+TARGET_SUPPORT_FACE_UNLOCK := true
+TARGET_INCLUDE_STOCK_ARCORE := false
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=mojito
+    PRODUCT_NAME=mojito \
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ1A.210105.003 7005429 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys
